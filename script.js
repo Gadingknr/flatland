51f1f25d627c72830c19876f677b0acd12a5bc36
Gading Kinarayana <100770471+Gadingknr@users.noreply.github.com>
2023-03-13 18:01:43 +0800

    need fixing for next week

--- script.js
+++ script.js
@@ -15,7 +15,7 @@ function createBuzzwordPhrase() {
     let buzz = ["Paradigm-changing", "Multi-tier", "10,000-foot", "Agile", "Customer", "Win-win"];
     let action = ["empowered", "value-added", "synergy", "creative", "oriented", "focused", "aligned"];
     let outcome = ["process", "deliverable", "solution", "tipping-point", "strategy", "vision"];
-
+/*note need fixing*/
     let idx_buz = Math.floor(Math.random() * buzz.length);
     let idx_act = Math.floor(Math.random() * action.length);
     let idx_out = Math.floor(Math.random() * outcome.length);
