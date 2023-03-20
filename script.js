93d8bff218d5f2a7b089a5808a1b26d68cf9eb37
Gading Kinarayana <100770471+Gadingknr@users.noreply.github.com>
2023-03-20 15:06:59 +0800

    fix

--- script.js
+++ script.js
@@ -7,8 +7,8 @@ function changeColour(colour) {
 square.addEventListener('click', (event) => changeColour('green'))
 
 /*create hover message*/
-square.addEventListener('click', (event) => changeColour('grey'));
-square.addEventListener('click', (event) => changeColour('red')):
+square.addEventListener('mouseover', (event) => changeColour('grey'))
+square.addEventListener('mouseover', (event) => changeColour('red'))
 
 function createBuzzwordPhrase() {
     /* See https://en.wikipedia.org/wiki/List_of_buzzwords */
